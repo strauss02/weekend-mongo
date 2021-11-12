f829f2196936511a78765e2f50bb648a55061ccd
Ido Strauss <strauss02@hotmail.co.il>
2021-11-12 13:25:22 +0200

    basic set up

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
-const app = express();
-require("dotenv").config();
-const userRouter = require("./routers/user");
-const { morganBodyLogger } = require("./morgan");
-const errorHandlingMiddleware = require("./middlewares/errorHandlingMiddleware");
-const unknownEndpoint = require("./middlewares/unknownEndpoint");
+/* ======================= */
 
-const port = process.env.PORT || 3000;
+require('dotenv').config()
 
-const mongo = process.env.DATABASE;
+/* ======================= */
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+const { morganBodyLogger } = require('./morgan')
+/* ======================= */
+const userRouter = require('./routers/user')
+const errorHandlingMiddleware = require('./middlewares/errorHandlingMiddleware')
+const unknownEndpoint = require('./middlewares/unknownEndpoint')
+/* ======================= */
+
+const mongo = process.env.MONGODB_URI
 
 //connecting to database
 mongoose
-    .connect(mongo)
-    .then((result) => {
-        console.log("connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log("error connecting to MongoDB:", error.message);
-    });
+  .connect(mongo)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-app.use(morganBodyLogger);
+/* ======================= */
 
-app.get("/", (req, res) => {
-    res.send("working");
-});
-app.use("/user", jsonParser, userRouter);
+app.use(morganBodyLogger)
+/* ======================= */
+
+app.get('/', (req, res) => {
+  res.send('working')
+})
+/* ======================= */
+
+app.use('/user', userRouter)
 
 // unknownEndpoint handling middleware
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 // error handling middleware
-app.use(errorHandlingMiddleware);
+app.use(errorHandlingMiddleware)
+
+/* ======================= */
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`litsening in port ${port}`);
-});
+  console.log(`litsening in port ${port}`)
+})
